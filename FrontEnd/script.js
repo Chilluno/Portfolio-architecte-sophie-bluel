3eea7ac4cee8edaf9fb2ff0ba32ae2fa175952b1
Wesley Slimani <wesley.slimani@gmail.com>
2023-03-07 02:45:31 +0100

    feat: replaced index.html works figures with the ones from the backend

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,2 +1,24 @@
-const worksResponse = await fetch("http://localhost:5678/api/works");
-const works = await worksResponse.json();
+let gallery = document.querySelector(".gallery");
+gallery.replaceChildren('');
+
+fetch("http://localhost:5678/api/works")
+.then(result => {
+  return result.json();
+})
+.then(data => {
+  data.forEach(data => {  
+  const dataURL = data.imageUrl.replace(/[0-9]/g, '');  
+  const markup = `<figure>
+  <img src="assets/${dataURL.slice(18)}" alt="${data.title}">
+  <figcaption>${data.title}</figcaption>
+  </figure>`;
+  console.log(markup);
+
+  gallery.insertAdjacentHTML('beforeend',markup);
+
+  });
+  
+})
+.catch(error => console.log(error));
+
+
