1a7b503584edea95bcca1f5c89e203795f0b97fb
Wesley Slimani <wesley.slimani@gmail.com>
2023-03-11 16:02:59 +0100

    feat: made filter buttons functional

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,26 +1,24 @@
-const getWorks = async() => {
+const getWorks = async () => {
   let works;
 
   const response = await fetch("http://localhost:5678/api/works");
 
   works = await response.json();
 
-  works.forEach((data) => {  
-    const dataURL = data.imageUrl.replace(/[0-9]/g, '');  
-    const markup = `<figure>
+  works.forEach((data) => {
+    const dataURL = data.imageUrl.replace(/[0-9]/g, "");
+    const markup = `<figure id="${data.categoryId}">
     <img src="assets/${dataURL.slice(18)}" alt="${data.title}">
     <figcaption>${data.title}</figcaption>
     </figure>`;
     console.log(markup);
-    document.querySelector(".gallery").insertAdjacentHTML('beforeend',markup);
-    })
+    document.querySelector(".gallery").insertAdjacentHTML("beforeend", markup);
+  });
 
-    return works;
-} 
+  return works;
+};
 
-
-
-const createFilterbuttons = async() => {
+const createFilterbuttons = async () => {
   let filters;
   let worksResults = await getWorks();
   const projectFilter = document.querySelector(".filters");
@@ -31,38 +29,42 @@ const createFilterbuttons = async() => {
 
   filters.forEach((filter) => {
     const filterButton = document.createElement("button");
+    filterButton.className = "filterbtn";
     filterButton.innerHTML = filter.name;
+    filterButton.setAttribute("id", filter.id);
     projectFilter.appendChild(filterButton);
   });
 
-  applyCategory(filters,worksResults);
+  applyCategory();
 
   console.log(filters);
   console.log(worksResults);
+};
 
-  
-}
-
-const applyCategory = (filter, projects) => {
-
-buttonHighlight();
-
-
-
-}
-
-const buttonHighlight = () => {
+const applyCategory = () => {
   buttons = Array.from(document.querySelectorAll(".filters button"));
+  projectWorks = document.querySelectorAll(".gallery figure");
 
-  buttons.forEach(button => {
-    button.addEventListener("click", function() {
-    buttons.forEach((button) => {
-      button.classList.remove("active");
-    })
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      buttons.forEach((button) => {
+        button.classList.remove("active");
+      });
       button.classList.add("active");
-    })
-});
-}
 
+      if (button.classList.contains("active")) {
+        projectWorks.forEach((project) => {
+          if (button.getAttribute("id") == project.getAttribute("id")) {
+            project.classList.remove("hidden");
+          } else if (button.getAttribute("id") == "*") {
+            project.classList.remove("hidden");
+          } else {
+            project.classList.add("hidden");
+          }
+        });
+      }
+    });
+  });
+};
 
 createFilterbuttons();
