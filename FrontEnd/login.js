0c5e7b917a7c769215ed24fa99a3a85aa858f0ff
Wesley Slimani <wesley.slimani@gmail.com>
2023-03-21 12:54:08 +0100

    fix: tweaked success condition when fetching the token

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,46 +1,39 @@
 const formLogin = document.querySelector("#login form");
 
 formLogin.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    event.preventDefault();
+  const formData = new FormData(formLogin);
+  const data = Object.fromEntries(formData);
 
-   const formData = new FormData(formLogin);
-   const data = Object.fromEntries(formData);
-
-
-   fetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+      "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(result => {
-    console.log('Success:', result);
-    const formError = document.createElement('p');
-    formError.innerText = "";
-    if(result.message){
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.token != null) {
+        console.log("Success:", data);
+        console.log("user is auth");
+        localStorage.setItem("token", data.token);
+        window.location.href = "index.html";
+      } else {
+        const formError = document.createElement("p");
+        formError.innerText = "";
         console.log("user is not found");
         formError.classList.add("error");
         formError.innerText = "Erreur dans l’identifiant ou le mot de passe.";
-        formLogin.appendChild(formError);
-    }
-    else{
-        console.log("user is auth");
-        localStorage.setItem('token', result.token);
-        window.location.href = "index.html";
-    }
-})
-.catch(error => {
-    console.error('Error:', error);
+        formLogin.parentNode.insertBefore(formError,formLogin.nextSibling);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
-
-
-})
-
-
 // loginBtn.addEventListener('click', (event) => {
-    // on met en place un évènement pour empêcher le comportement par défaut du bouton
+// on met en place un évènement pour empêcher le comportement par défaut du bouton
 //    event.preventDefault();
