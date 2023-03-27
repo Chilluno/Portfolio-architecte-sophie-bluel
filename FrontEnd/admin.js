f4605e7a10528b14f8080db98e66b4a19fcc57a3
Wesley Slimani <wesley.slimani@gmail.com>
2023-03-27 18:13:11 +0200

    feat: added fetch request on delete buttons

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -1,55 +1,55 @@
-console.log(localStorage.getItem('token'));
+console.log(localStorage.getItem("token"));
 
-if(localStorage.getItem('token')){
-   const headerLogin = document.querySelector('header ul').children[2];
-   headerLogin.innerText = "logout";
-   const editBanner = document.createElement('div');
-   const header = document.querySelector("header");
-   editBanner.innerHTML = `<i class="fa-solid fa-pen-to-square"></i><p>Mode édition</p><button class="bannerbtn">publier les changements</button>`;
- 
-   editBanner.classList.add("banner");
-   header.before(editBanner);
-   
- 
-   const editButton = `<div class="editbtn"><i class="fa-solid fa-pen-to-square fa-xl"></i><p>modifier</p></div>`;
-   document.querySelector("#portfolio h2").insertAdjacentHTML("afterend",editButton);
-   document.querySelector("#introduction img").insertAdjacentHTML("afterend",editButton);
-   document.querySelector(".filters").classList.add("hidden");
- 
-   const editGallery = document.querySelector("#portfolio .editbtn");
-   const galleryModal = document.getElementById("modal");
-   const closeModalBtn = document.querySelector(".close");
-   const modalContent = document.querySelector(".modal-content");
-   const galleryImages = document.getElementById("portfolio").getElementsByTagName("img");
-  
-   
-   editGallery.addEventListener("click", () =>{
+if (localStorage.getItem("token")) {
+  const headerLogin = document.querySelector("header ul").children[2];
+  headerLogin.innerText = "logout";
+  const editBanner = document.createElement("div");
+  const header = document.querySelector("header");
+  editBanner.innerHTML = `<i class="fa-solid fa-pen-to-square"></i><p>Mode édition</p><button class="bannerbtn">publier les changements</button>`;
 
-     galleryModal.classList.remove("hidden");
+  editBanner.classList.add("banner");
+  header.before(editBanner);
 
-     const imageArray = [...galleryImages];
-     let imageId = 0;
+  const editButton = `<div class="editbtn"><i class="fa-solid fa-pen-to-square fa-xl"></i><p>modifier</p></div>`;
+  document
+    .querySelector("#portfolio h2")
+    .insertAdjacentHTML("afterend", editButton);
+  document
+    .querySelector("#introduction img")
+    .insertAdjacentHTML("afterend", editButton);
+  document.querySelector(".filters").classList.add("hidden");
 
-      imageArray.forEach(image => {
-      
-      imageId++;  
+  const editGallery = document.querySelector("#portfolio .editbtn");
+  const galleryModal = document.getElementById("modal");
+  const closeModalBtn = document.querySelector(".close");
+  const modalContent = document.querySelector(".modal-content");
+  const galleryImages = document
+    .getElementById("portfolio")
+    .getElementsByTagName("img");
+  const deleteButtons = document.getElementsByClassName("deletebtn");
+
+  editGallery.addEventListener("click", () => {
+    galleryModal.classList.remove("hidden");
+
+    const imageArray = [...galleryImages];
+    let imageId = 0;
+
+    imageArray.forEach((image) => {
+      imageId++;
 
       const imageDiv = document.createElement("div");
-      imageDiv.setAttribute("id", imageId.toString());
       imageDiv.classList.add("modal-image");
       imageDiv.innerHTML = image.outerHTML;
 
       const deleteButton = document.createElement("div");
+      deleteButton.setAttribute("id", imageId.toString());
       deleteButton.classList.add("deletebtn");
       deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
 
-      
-      imageDiv.insertAdjacentText("beforeend","éditer");
-      deleteButton.addEventListener("click", (imageId) => {
-        //const activeImage = document.querySelectorAll(".modal-image");
-        //console.log(activeImage[imageId]);
-      });
+      imageDiv.insertAdjacentText("beforeend", "éditer");
+
       imageDiv.appendChild(deleteButton);
+
       imageDiv.addEventListener("mouseover", () => {
         const moveButton = document.createElement("div");
         moveButton.classList.add("movebtn");
@@ -61,41 +61,46 @@ if(localStorage.getItem('token')){
         document.querySelector(".movebtn").remove();
       });
 
+      modalContent.appendChild(imageDiv);
+    });
 
-      modalContent.appendChild(imageDiv); 
-      
-      
+    Array.from(deleteButtons).forEach((button) => {
+      button.addEventListener("click", () => {
+        console.log(button.getAttribute("id"));
 
-     })
-   })
+        const token = localStorage.getItem("token");
+        console.log(token);
 
-   
-  
-   closeModalBtn.addEventListener("click", () => {
-     galleryModal.classList.add("hidden");
-     modalContent.innerHTML = "";
+        fetch("http://localhost:5678/api/works/" + button.getAttribute("id"), {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      });
+    });
+  });
 
-   })
- 
-   /*
-   window.onclick = (event) => {
-     if(event.target == galleryModal){
-       galleryModal.classList.add("hidden");
-       modalContent.innerHTML = "";
-     }
-   }*/
- 
-   headerLogin.addEventListener("click", () => {
-     localStorage.clear();
-     window.location.reload();
-   })
+  closeModalBtn.addEventListener("click", () => {
+    galleryModal.classList.add("hidden");
+    modalContent.innerHTML = "";
+  });
 
- }
+  window.onclick = (event) => {
+    if (event.target == galleryModal) {
+      galleryModal.classList.add("hidden");
+      modalContent.innerHTML = "";
+    }
+  };
 
- else{
-   const headerLogin = document.querySelector('header ul').children[2];
-   headerLogin.addEventListener("click", () => {
-   window.location.href = "login.html";
-   })  
- }
-
+  headerLogin.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.reload();
+  });
+} else {
+  const headerLogin = document.querySelector("header ul").children[2];
+  headerLogin.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+}
