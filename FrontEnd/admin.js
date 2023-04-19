a90069f945d69e95afa6349bc2aa67e344052235
Wesley Slimani <wesley.slimani@gmail.com>
2023-04-19 10:03:49 +0200

    fix: fixed form errors

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -6,63 +6,66 @@ const allCategories = await fetch("http://localhost:5678/api/categories").then(
   (response) => response.json()
 );
 
-
 console.log(localStorage.getItem("token"));
 console.log(allCategories);
 
 const getWorks = (works) => {
-
-
- works.forEach((data) => {
-  const dataURL = data.imageUrl.replace(/[0-9]/g, "");
-  const markup = `<figure id="${data.categoryId}" class="modal-image">
-  <img id="${data.id}" src="assets/${dataURL.slice(18)}" alt="${data.title}">
+  works.forEach((data) => {
+    const markup = `<figure id="${data.categoryId}" class="modal-image">
+  <img id="${data.id}" src=${data.imageUrl} alt="${data.title}">
   <figcaption>éditer</figcaption>
-  <div id="${data.id}" class="deletebtn"><i class="fa-solid fa-trash-can"></i></div>
+  <div id="${
+    data.id
+  }" class="deletebtn"><i class="fa-solid fa-trash-can"></i></div>
   </figure>`;
-  
-  document.querySelector(".modal-content").insertAdjacentHTML("beforeend", markup);
-      
-  document.querySelectorAll(".modal-image").forEach((image) =>{
-    image.addEventListener("mouseover", () => {
-      const moveButton = document.createElement("div");
-      moveButton.classList.add("movebtn");
-      moveButton.innerHTML = `<i class="fa-solid fa-up-down-left-right"></i>`;
-      image.appendChild(moveButton)
+
+    document
+      .querySelector(".modal-content")
+      .insertAdjacentHTML("beforeend", markup);
+
+    document.querySelectorAll(".modal-image").forEach((image) => {
+      image.addEventListener("mouseover", () => {
+        const moveButton = document.createElement("div");
+        moveButton.classList.add("movebtn");
+        moveButton.innerHTML = `<i class="fa-solid fa-up-down-left-right"></i>`;
+        image.appendChild(moveButton);
+      });
+    });
+
+    document.querySelectorAll(".modal-image").forEach((image) => {
+      image.addEventListener("mouseout", () => {
+        document.querySelector(".movebtn").remove();
+      });
     });
   });
-
-  document.querySelectorAll(".modal-image").forEach((image) =>{
-    image.addEventListener("mouseout", () => {
-      document.querySelector(".movebtn").remove();
-    });
-  });
-
- }) 
-}
+};
 
 const deleteWorks = () => {
-
   document.querySelectorAll(".deletebtn").forEach((deletebtn) => {
     deletebtn.addEventListener("click", (e) => {
       e.preventDefault();
       const Id = deletebtn.getAttribute("id");
-      console.log(deletebtn.getAttribute("id"));
 
       const token = localStorage.getItem("token");
-      console.log(token);
+      deletebtn.parentElement.classList.add("hidden");
+      
+      const galleryWork = document.querySelectorAll(`[src=${CSS.escape(deletebtn.parentElement.children[0].src)}`);
 
+        galleryWork.forEach((work) => {
+          console.log(work.parentNode);
+          work.parentNode.remove();
+        });
       
       fetch(`http://localhost:5678/api/works/${Id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
-      });
-  })
-})
-}
+      })
+    });
+  });
+};
 
 if (localStorage.getItem("token")) {
   const headerLogin = document.querySelector("header ul").children[2];
@@ -90,33 +93,31 @@ if (localStorage.getItem("token")) {
   const modalBtn = document.querySelector(".modalbtn");
   const backBtn = document.querySelector(".backbtn");
 
+  
+
   editGallery.addEventListener("click", () => {
     galleryModal.classList.remove("hidden");
 
-      getWorks(allWorks);
-      deleteWorks();
+    getWorks(allWorks);
+    deleteWorks();
+    
 
-    modalBtn.addEventListener("click", () =>{
-      
+    modalBtn.addEventListener("click", () => {
       document.querySelector(".modal-works").classList.add("hidden");
       document.querySelector(".modal-create").classList.remove("hidden");
-
     });
 
     backBtn.addEventListener("click", () => {
-
       document.querySelector(".modal-create").classList.add("hidden");
       document.querySelector(".modal-works").classList.remove("hidden");
-
-    })
-
+    });
   });
 
-  closeModalBtn.forEach(closebtn => {
-      closebtn.addEventListener("click", () => {
+  closeModalBtn.forEach((closebtn) => {
+    closebtn.addEventListener("click", () => {
       galleryModal.classList.add("hidden");
       modalContent.innerHTML = "";
-  })
+    });
   });
 
   window.onclick = (event) => {
@@ -140,35 +141,89 @@ if (localStorage.getItem("token")) {
 const form = document.querySelector(".create-work-form");
 
 const createWork = (event) => {
-event.preventDefault();
-const token = localStorage.getItem("token");
+  event.preventDefault();
+  const token = localStorage.getItem("token");
 
-const formData = new formData();
+  const formData = new FormData();
 
-formData.append("title",document.getElementById("title").value);
-formData.append("category",+document.getElementById("category").value);
-formData.append("image",document.getElementById("image").files[0]);
+  formData.append("title", document.getElementById("title").value);
+  formData.append("category", +document.getElementById("category").value);
+  formData.append("image", document.getElementById("image").files[0]);
 
+  console.log(document.getElementById("image").files[0]);
 
-fetch("http://localhost:5678/api/works", {
-  method: "POST",
-  headers: {
-    Authorization: `Bearer ${token}`
-  },
-  body: formData
-})
-.then(data => console.log(data))
-.catch(error => console.error(error))
+  fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  })
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+    
 }
-
-form.addEventListener("submit", createWork);
 
 const optionList = document.querySelector("select");
 
-allCategories.forEach(category => {
+allCategories.forEach((category) => {
   const categoryOption = document.createElement("option");
   categoryOption.setAttribute("value", `${category.id}`);
-  categoryOption.innerHTML= category.name;
+  categoryOption.innerHTML = category.name;
   optionList.appendChild(categoryOption);
   console.log(categoryOption);
-})
+});
+
+const submitButton = document.getElementById("creatework");
+const fileInput = document.getElementById("image");
+const titleInput = document.getElementById("title");
+
+form.addEventListener("change", () => {
+  if(fileInput.value == "" || titleInput.value == ""){
+
+  if(!document.querySelector("error")){
+    const formError = document.getElementById("error");
+    formError.innerText = "";
+    formError.classList.add("error");
+    formError.innerText = "Un ou plusieurs champs sont manquant";
+  }
+    submitButton.disabled = true;
+  }
+  else{
+    const formError = document.getElementById("error");
+    formError.innerText = "";
+    submitButton.removeEventListener("click", formError);
+    form.addEventListener("submit", createWork);
+    submitButton.disabled = false;
+  }
+  
+});
+
+const imgAddPhoto = document.getElementById("submit-img");
+
+const displayImage = (e) => {
+  imgAddPhoto.classList.remove("hidden");
+  if (e.target.files[0]) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+          imgAddPhoto.setAttribute('src', reader.result);
+      });        reader.readAsDataURL(e.target.files[0]);
+
+      document.querySelector(".submit-img-wrapper label").classList.add("opaque");
+      document.querySelector(".submit-img-wrapper i").classList.add("opaque");
+      document.querySelector(".submit-img-wrapper span").classList.add("opaque");
+  }
+};
+
+fileInput.addEventListener("change", displayImage);
+
+ 
+
+
+
+
+
+
+
+
+
